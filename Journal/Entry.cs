d58134d78ecaa854cd65b09e930fc2060cc1b5b1
Alex <Alex@ALEXCOMPUTER>
2023-03-21 16:11:09 -0400

    Add project files.

---
+++ Journal/Entry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth.Background;
+
+namespace Journal
+{
+    internal class Entry
+    {
+        DateTime date;
+
+        string text;
+
+        public Entry(string text, DateTime date)
+        {
+            this.text = text;
+            this.date = date;
+        }
+
+        public String GetText() { return this.text; }
+
+        public void SetText(string text) { this.text = text;}
+
+        public DateTime GetDate() { return this.date; }
+        public void SetDate(DateTime date) { this.date = date; }
+
+
+    }
+}
---
+++ Journal/JournalManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Journal
+{
+    internal class JournalManager
+    {
+        private Dictionary<DateOnly, Entry> entries;
+
+        private static JournalManager instance = new JournalManager();
+
+        private JournalManager()
+        {
+            entries = new Dictionary<DateOnly, Entry>();
+        }
+
+        public static JournalManager GetInstance()
+        {
+            return instance;
+        }
+
+
+        public void AddEntry(Entry entry)
+        {
+            try { 
+                entries.Add(DateOnly.FromDateTime(entry.GetDate()), entry); }
+            catch(ArgumentException e)
+            {
+                
+            }
+            
+        }
+
+
+        public void Clear()
+        {
+            entries.Clear();
+        }
+        
+        public Entry GetEntry(DateTime date)
+        {
+            return entries[DateOnly.FromDateTime(date)];
+        }
+
+        public Entry GetEntry(DateOnly date)
+        {
+            return entries[date];
+        }
+
+    }
+}
